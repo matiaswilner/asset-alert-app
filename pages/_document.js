b43c71387fe1a115433577faea42183a871996c2
matiaswilner <57733384+matiaswilner@users.noreply.github.com>
2026-04-13 21:48:07 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default function Document() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Assetic" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </Head>
       <body>
         <Main />
